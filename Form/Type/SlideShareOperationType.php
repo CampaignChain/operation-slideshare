5b8f7c4931ecfc89ca31e62ee2ccd99068a974c9
Sandro Groganz <sandro@campaignchain.com>
2015-10-01 14:48:59 +0200

    CE-382 Adjust all modules to work with new modules framework

--- Form/Type/SlideShareOperationType.php
+++ Form/Type/SlideShareOperationType.php
@@ -10,49 +10,16 @@
 
 namespace CampaignChain\Operation\SlideShareBundle\Form\Type;
 
-use Symfony\Component\Form\AbstractType;
+use CampaignChain\CoreBundle\Form\Type\OperationType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Doctrine\ORM\EntityManager;
-use Symfony\Component\Form\FormView;
-use Symfony\Component\Form\FormInterface;
 
-class SlideShareOperationType extends AbstractType
+class SlideShareOperationType extends OperationType
 {
-    private $status;
-    private $view = 'default';
-    protected $em;
-    protected $container;
-    private $location;
-    private $slideshows;
-
-    public function __construct(EntityManager $em, ContainerInterface $container)
-    {
-        $this->em = $em;
-        $this->container = $container;
-    }
-
-    public function setStatus($status){
-        $this->status = $status;
-    }
-
-    public function setView($view){
-        $this->view = $view;
-    }
-
-    public function setLocation($location){
-        $this->location = $location;
-    }
-
-    public function setSlideshows($slideshows){
-        $this->slideshows = $slideshows;
-    }
-    
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('slideshow', 'choice', array(
-            'choices'   => $this->slideshows,
+            'choices'   => $this->operationDetail,
             'required'  => true,
             'label' => false,
             'attr' => array(
@@ -60,18 +27,6 @@ class SlideShareOperationType extends AbstractType
                 'help_text' => 'Only slideshows that are private are being listed.',
             ),
         ));     
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function buildView(FormView $view, FormInterface $form, array $options)
-    {
-        if($this->location){
-            $view->vars['location'] = $this->location;
-        } else {
-            $view->vars['location'] = $options['data']->getOperation()->getActivity()->getLocation();
-        }
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
